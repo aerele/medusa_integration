f1bb8aaaf0ecf5f67ee67e45d3e4c35067092c60
l0gesh29 <logeshperiyasamy24@gmail.com>
2024-05-24 12:03:12 +0530

    feat: token expiry added

--- medusa_integration/constants.py
+++ medusa_integration/constants.py
@@ -1,12 +1,13 @@
 import frappe
 
-
 def get_url():
 	doc = frappe.get_doc("Medusa Configuration", "Medusa Configuration")
 	return doc.url, doc.enable
 
-def get_headers(with_token=False):
+def get_headers(with_token=False, expired=False):
 	doc = frappe.get_doc("Medusa Configuration", "Medusa Configuration")
+	if expired:
+		doc.get_access_token()
 	if with_token and doc.access_token:
 		return {
 					'Authorization': 'Bearer '+ doc.access_token,
