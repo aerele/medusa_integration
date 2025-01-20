6eb49217f0ac204a032eb2611808b6f599f9d456
chethank1407 <chethank1407@gmail.com>
2025-01-20 15:08:45 +0400

    init: recommended items list

---
+++ medusa_integration/medusa_integration/doctype/recommended_items_list/test_recommended_items_list.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Aerele Technologies and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestRecommendedItemsList(FrappeTestCase):
+	pass
