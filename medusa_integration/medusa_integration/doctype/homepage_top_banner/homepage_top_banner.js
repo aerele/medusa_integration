cc56dbbc46e9fbfb8090c61e745c2c7203cb15ec
chethank1407 <chethank1407@gmail.com>
2025-01-20 14:12:55 +0400

    feat: created list of entries

---
+++ medusa_integration/medusa_integration/doctype/homepage_top_banner/homepage_top_banner.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Aerele Technologies and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Homepage Top Banner', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ medusa_integration/medusa_integration/doctype/homepage_top_banner/test_homepage_top_banner.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Aerele Technologies and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestHomepageTopBanner(FrappeTestCase):
+	pass
