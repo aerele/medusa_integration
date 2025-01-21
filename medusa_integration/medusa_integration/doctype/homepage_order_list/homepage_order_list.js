23d72ed9101c20a258f0b55d6a3e36f4272b9e96
chethank1407 <chethank1407@gmail.com>
2025-01-21 14:47:38 +0400

    del: unused doctypes

--- medusa_integration/medusa_integration/doctype/homepage_order_list/homepage_order_list.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, Aerele Technologies and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Homepage Order List', {
-	// refresh: function(frm) {
-
-	// }
-});
--- medusa_integration/medusa_integration/doctype/homepage_order_list/test_homepage_order_list.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Aerele Technologies and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestHomepageOrderList(FrappeTestCase):
-	pass
