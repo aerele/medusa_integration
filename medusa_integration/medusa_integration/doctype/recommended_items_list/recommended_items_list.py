ca3305141aa5b0efcec91dc3afe67944a869a123
chethank1407 <chethank1407@gmail.com>
2025-01-21 15:16:00 +0400

    del: removed unused doctype

--- medusa_integration/medusa_integration/doctype/recommended_items_list/recommended_items_list.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2025, Aerele Technologies and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class RecommendedItemsList(Document):
-	pass
