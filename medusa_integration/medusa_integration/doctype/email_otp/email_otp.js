4c218189cafe64a63676412a45be8ec3fc33f282
chethank1407 <chethank1407@gmail.com>
2025-02-10 13:05:37 +0530

    init: email otps

---
+++ medusa_integration/medusa_integration/doctype/email_otp/email_otp.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Aerele Technologies and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Email OTP', {
+	// refresh: function(frm) {
+
+	// }
+});
