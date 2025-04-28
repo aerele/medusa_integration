2de5892e6b3bb23b38f31766a939472421b66adc
chethank1407 <chethank1407@gmail.com>
2025-04-28 10:41:41 +0530

    feat: dynamic handling of items

--- medusa_integration/medusa_integration/doctype/expiring_items/expiring_items.py
+++ medusa_integration/medusa_integration/doctype/expiring_items/expiring_items.py
@@ -1,8 +1,25 @@
 # Copyright (c) 2025, Aerele Technologies and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class ExpiringItems(Document):
-	pass
+	def validate(self):
+		homepage_doc = frappe.get_doc("Homepage Landing", "Active Homepage Landing")
+		
+		selected_items = {row.website_item for row in self.expiring_items if row.show}
+
+		existing_clearance_items = {row.website_item: row for row in homepage_doc.clearance_items}
+
+		homepage_doc.clearance_items = [
+			row for row in homepage_doc.clearance_items if row.website_item in selected_items
+		]
+
+		homepage_existing_items_set = {row.website_item for row in homepage_doc.clearance_items}
+
+		for item in selected_items:
+			if item not in homepage_existing_items_set:
+				homepage_doc.append("clearance_items", {"website_item": item})
+
+		homepage_doc.save()
