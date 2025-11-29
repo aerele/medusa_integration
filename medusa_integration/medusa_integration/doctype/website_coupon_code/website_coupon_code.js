34a190f5bcc56b25c16db8ff966a2e8c94edcd34
chethank1407 <chethank1407@gmail.com>
2025-11-29 12:35:40 +0530

    feat: created coupon feature

---
+++ medusa_integration/medusa_integration/doctype/website_coupon_code/website_coupon_code.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Aerele Technologies and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Website Coupon Code', {
+	// refresh: function(frm) {
+
+	// }
+});
